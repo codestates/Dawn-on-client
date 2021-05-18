fcaa4037541cff2e68580ca8ba3b2e817fcea644
allofhyuk <gggg21367@gmail.com>
2021-05-18 10:24:41 +0900

    구현: 랜딩페이지 스크롤 이벤트

--- dawn-on-client/src/pages/Landing.tsx
+++ dawn-on-client/src/pages/Landing.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import styled from '@emotion/styled';
+import Login from '../components/Login';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function Landing() {
-  return <div>여기는 첫 화면으로 보여질 랜딩페이지입니다</div>;
+  const LandingSection = styled.div`
+    height: 100vh;
+    color: #000;
+    text-align: center;
+    font-size: 50px;
+    transition: transform 0.7s, opacity 1s;
+  `
+  const Button = styled.button`
+    position: fixed;
+    top:95%;
+    left: 95%;
+    border-radius: 76px;
+    outline: none;
+    border:none;
+    background-color: #2b3390;
+    color: #faee9d;
+    width: 40px;
+  `
+  // Scroll animation
+  function isElementUnderBottom(elem:Element, triggerDiff:number) {
+    const { top } = elem.getBoundingClientRect();
+    const { innerHeight } = window;
+    return top > innerHeight + (triggerDiff || 0);
+  }
+  function handleScroll() {
+    const elems = document.querySelectorAll<HTMLElement>('.up-on-scroll');
+    elems.forEach(elem => {
+      if (isElementUnderBottom(elem, -20)) {
+        elem.style.opacity = "0";
+        elem.style.transform = 'translateY(100px)';
+      } else {
+        elem.style.opacity = "1";
+        elem.style.transform = 'translateY(0px)';
+      }
+    })
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  
+  return (
+  <>
+    {/* <Login /> */}
+    <LandingSection className="up-on-scroll">
+      section01
+    </LandingSection>
+    <LandingSection className="up-on-scroll">
+      section02
+    </LandingSection>
+    <LandingSection className="up-on-scroll">
+       section03
+    </LandingSection>
+    <LandingSection className="up-on-scroll">
+       section04
+    </LandingSection>
+    <LandingSection className="up-on-scroll">
+       section05
+    </LandingSection>
+    <Button onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>Top</Button>
+  </>
+  );
 }
 
 export default Landing;
