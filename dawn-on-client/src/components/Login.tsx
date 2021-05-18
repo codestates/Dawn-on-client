cfed473c2e7b459c5d7c3196f41c7d319ef45dc4
Jeong-HW <gusdnd114@naver.com>
2021-05-18 10:12:13 +0900

    구현:로그인 창 기본구조 설계

--- dawn-on-client/src/components/Login.tsx
+++ dawn-on-client/src/components/Login.tsx
@@ -1,1 +1,24 @@
 import React from "react";
+import "../App.css";
+
+type LoginProps = {
+  LoginModal: boolean;
+  closeModal: any;
+};
+
+function Login({ LoginModal, closeModal }: LoginProps) {
+  return (
+    <>
+      {LoginModal ? (
+        <div>
+          <div id="login-modal">
+            로그인 모달창 들어올 곳
+            <button onClick={() => closeModal()}>닫기</button>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+export default Login;
