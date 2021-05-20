18cb9934b54e15cfb9abb933d06cb6b8cd94c3e8
Jeong-HW <gusdnd114@naver.com>
2021-05-20 16:35:08 +0900

    수정: Footer 전체 페이지 랜더링 -> Landing 페이지 에서만 랜더링

--- dawn-on-client/src/App.tsx
+++ dawn-on-client/src/App.tsx
@@ -1,9 +1,7 @@
 import Nav from "./components/Nav";
-import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
 import Explore from "./pages/Explore";
 import MyFeed from "./pages/MyFeed";
-import UserFeed from "./pages/UserFeed";
 import CustomPlanner from "./pages/CustomPlanner";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
@@ -17,11 +15,9 @@ function App() {
           <Route exact path="/" render={() => <Landing />} />
           <Route path="/explore" render={() => <Explore />} />
           <Route path="/myfeed" render={() => <MyFeed />} />
-          <Route path="/userfeed" render={() => <UserFeed />} />
           <Route path="/custom-planner" render={() => <CustomPlanner />} />
         </Switch>
       </Router>
-      <Footer />
     </>
   );
 }
