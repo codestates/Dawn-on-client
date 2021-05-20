f16b4932638ff89ae71d6aa103b11e81d2fa45fc
allofhyuk <gggg21367@gmail.com>
2021-05-20 16:38:27 +0900

    구현: table  드래그 이벤트 구현

--- dawn-on-client/src/pages/CustomPlanner.tsx
+++ dawn-on-client/src/pages/CustomPlanner.tsx
@@ -1,9 +1,17 @@
 // 플래너를 작성 후 게시물 업로드하는 페이지
 // PlannerForm , CustomBar
 import React from "react";
+import PlannerFormDnd from "../components/PlannerFormDnd";
+import CustomBar from "../components/CustomBar";
+import PlannerForm from "../components/PlannerForm";
 
 function CustomPlanner() {
-  return <div>이 곳은 플래너를 작성하고 업로드할 공간입니다</div>;
+  return (
+    <>
+      <PlannerForm />
+      <CustomBar />
+    </>
+  );
 }
 
 export default CustomPlanner;
