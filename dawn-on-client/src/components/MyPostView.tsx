a888e04e7cd689fc1bdc7acc6ad3a84ba12e4b5c
Jeong-HW <gusdnd114@naver.com>
2021-05-23 18:25:19 +0900

    구현: 개인 페이지에서 게시물 박스 클릭시 오른쪽에 보여질 상세 페이지

--- dawn-on-client/src/components/MyPostView.tsx
+++ dawn-on-client/src/components/MyPostView.tsx
@@ -1,2 +1,14 @@
 import React from "react";
-// 플래너 박스 클릭 시 보여지는 상세 페이지 뷰어
+
+//Edit 버튼 추가. 커스텀 플래너 페이지로 이동한다 (여기서, 해당 게시물에 대한 정보도 함께 들고 간다)
+//MyPost 컴포넌트에서 클릭 이벤트로 Redux에 저장된 해당 게시물의 데이터를 useSelector로 가져와 랜더링한다
+function MyPostView() {
+  return (
+    <div id="MyPostView-container">
+      MyPostList 안의 MyPost 클릭 시 오른쪽에 상세 페이지로 보여질 공간입니다
+      <button>Edit</button>
+    </div>
+  );
+}
+
+export default MyPostView;
