4a81164e5d3ed4cd5ba15dfd328357c6dc4971c7
allofhyuk <gggg21367@gmail.com>
2021-05-23 18:24:16 +0900

    업데이트: initial setting

--- dawn-on-client/src/components/CustomBar.tsx
+++ dawn-on-client/src/components/CustomBar.tsx
@@ -1,12 +1,7 @@
-import React from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import "../css/custombar.css"
 
 function CustomBar () {
 
-  const onDragEnd = function() {
-    console.log('Drag ended');
-  }
 
   return(
     <div id="custombar-container">
