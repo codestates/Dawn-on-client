b0fd2975291df67634b30ab661fdb7bc11e41e18
allofhyuk <gggg21367@gmail.com>
2021-05-24 13:46:41 +0900

    업데이트: 리듀서 추가

--- dawn-on-client/src/module/index.tsx
+++ dawn-on-client/src/module/index.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import isLoginReducer from "./isLogin";
+import addColumnReducer from "./dragging";
 
 const rootReducer = combineReducers({
   isLoginReducer,
+  addColumnReducer
 });
 
 export default rootReducer;
