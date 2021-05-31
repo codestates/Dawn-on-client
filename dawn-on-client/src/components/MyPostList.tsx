871fbf04e60a8b2970c0a888009fff8873a9cb87
Jeong-HW <gusdnd114@naver.com>
2021-05-31 20:52:20 +0900

    구현: 게시물 데이터 유무에 따른 조건부 랜더링

--- dawn-on-client/src/components/MyPostList.tsx
+++ dawn-on-client/src/components/MyPostList.tsx
@@ -1,22 +1,39 @@
 import React from "react";
 import MyPost from "./MyPost";
+import empty_folder from "../img/empty_folder.png";
 
-//axios요청으로 게시물 목록을 불러와 map함수를 이용하여 렌더링한다
-function MyPostList() {
+// map함수를 이용하여 게시물 목록을 각각 하나씩 렌더링한다
+// 인피니티 스크롤사용...
+type MyPostListProps = {
+  postDatas: Array<any>;
+};
+
+function MyPostList({ postDatas }: MyPostListProps) {
+  console.log("게시물 목록에 전달받은 데이터: ", postDatas);
   return (
     <div id="MyPostList-container">
-      <div id="MyPostList-subtitle">목록</div>
-      <div id="MyPostList-posts">
-        <MyPost />
-        <MyPost />
-        <MyPost />
-        <MyPost />
-        <MyPost />
-        <MyPost />
-        <MyPost />
-        <MyPost />
-        <MyPost />
-      </div>
+      <div id="MyPostList-subtitle">내 게시물 목록</div>
+      {postDatas.length === 0 ? (
+        // <div id="postDatas-empty">
+        //   <img alt="empty_img" src={empty_folder} />
+        //   <div id="postDatas-empty-comment">목록이 비어있습니다</div>
+        // </div>
+        <div id="MyPostList-posts">
+          <MyPost postData={postDatas} />
+          <MyPost postData={postDatas} />
+          <MyPost postData={postDatas} />
+          <MyPost postData={postDatas} />
+          <MyPost postData={postDatas} />
+          <MyPost postData={postDatas} />
+          <MyPost postData={postDatas} />
+        </div>
+      ) : (
+        <div id="MyPostList-posts">
+          {postDatas.map((post) => (
+            <MyPost key={post.id} postData={post} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
