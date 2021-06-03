50ac819f12d006c2cfec84c39dada1e96f57e709
Jeong-HW <gusdnd114@naver.com>
2021-06-04 02:19:36 +0900

    업데이트: 임시로 Redux에  데이터 저장시켜놓을 변수 선언

--- dawn-on-client/src/module/initialState.tsx
+++ dawn-on-client/src/module/initialState.tsx
@@ -3,57 +3,88 @@ const initialState = {
   isEditProfile: false,
   subject_label: [
     {
-      subject: '국어',
-      color: '#ecf0f1',
+      subject: "국어",
+      color: "#ecf0f1",
     },
     {
-      subject: '수학',
-      color: '#ecf0f1',
+      subject: "수학",
+      color: "#ecf0f1",
     },
   ],
   plannerDatas: {
     date: "2021-05-18",
     dday: 50,
     today_learning_time: 0,
-    memo:"수능만점 받을거야아아앍",
+    memo: "수능만점 받을거야아아앍",
     comment: "힘들어..",
-    hour:"6h30m",
+    hour: "6h30m",
     back_color: "#fff",
     todos: [
       {
-          id: '1',
-          learning_time: 1,
-          box_color: "#ecf0f1",
-          todo_comment: '오늘 해야할 공부는 이거야',
-          subject: '국어',
-          start_time: "07:00",
-          checked: false,
+        id: "1",
+        learning_time: 1,
+        box_color: "#ecf0f1",
+        todo_comment: "오늘 해야할 공부는 이거야",
+        subject: "국어",
+        start_time: "07:00",
+        checked: false,
       },
       {
-          id: '2',
-          learning_time: 2,
-          box_color: "#ecf0f1",
-          todo_comment: '오늘 해야할 공부는 이거야',
-          subject: '수학',
-          start_time: "08:00",
-          checked: true,
+        id: "2",
+        learning_time: 2,
+        box_color: "#ecf0f1",
+        todo_comment: "오늘 해야할 공부는 이거야",
+        subject: "수학",
+        start_time: "08:00",
+        checked: true,
       },
     ],
-    tags : [
+    tags: [
       {
-          "id": 1,
-          "tag": "취준"
+        id: 1,
+        tag: "취준",
       },
       {
-          "id": 2,
-          "tag": "개발"
+        id: 2,
+        tag: "개발",
       },
       {
-          "id": 3,
-          "tag": "코딩"
-      }
-  ]
-  }
+        id: 3,
+        tag: "코딩",
+      },
+    ],
+  },
+  click_postview: Object,
+  click_exploreview: Object,
+  ExploreList: [],
+  MyFeedList: [],
+  MyFeedinfo: {
+    user_nickname: "",
+    user_img: "",
+    user_job: "",
+    profile_comment: "",
+    total_learning_time: 0,
+    total_posting: 0,
+  },
+  ranking_first: {
+    user_img: "",
+    user_nickname: "",
+    total_thumbsup: 0,
+  },
+  ranking_second: {
+    user_img: "",
+    user_nickname: "",
+    total_thumbsup: 0,
+  },
+  ranking_third: {
+    user_img: "",
+    user_nickname: "",
+    total_thumbsup: 0,
+  },
+  MyPostThumbsUp: Boolean,
+  ExploreThumbsUp: Boolean,
+  MyPostThumbsUpCount: Number,
+  ExploreThumbsUpCount: Number,
 };
 
 export default initialState;
