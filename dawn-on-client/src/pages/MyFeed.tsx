2a76e6cde093fa59a5bb16d03386161f74ee3ede
Jeong-HW <gusdnd114@naver.com>
2021-06-04 02:17:07 +0900

    수정: GET요청 데이터 Redux로 저장

--- dawn-on-client/src/pages/MyFeed.tsx
+++ dawn-on-client/src/pages/MyFeed.tsx
@@ -1,51 +1,49 @@
-/* 나의 피드 페이지
-1. MyProfileSidebar  ( 왼쪽 : 내 개인 피드에서 보여지는 기본 정보 ) 
-2. EditProfile ( “정보수정” 버튼 클릭 시, MyPostList 와 MyPostView가 차지하고 있는 컴포넌트가 사라지고 그 자리에 랜더링 )
-    1. Edit 취소 버튼 생성해야함
-    2. 프로필 사진 , 닉네임 ,코멘트, 직업군, 비밀번호(비밀번호 확인), / 소셜 로그인은 프로필과 비밀번호 변경 불가
-3. MyPostList ( 중앙 : 내가 작성한 게시물들의 썸네일 박스의 썸네일들이 보여지는 리스트 ) 
-    1. MyPost ( 중앙 : 썸네일 리스트에 담겨있는 내가 작성한 각각의 플래너 박스  )  👉 [ MyPostList ]
-        1. Delete 버튼 O
-4. MyPostView ( 오른쪽 : 플래너 박스 클릭 시 , 보여주는 상세 페이지 )
-    1. Edit 버튼 O
-*/
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "../css/myfeed.css";
 import MyProfileSidebar from "../components/MyProfileSidebar";
 import MyPostList from "../components/MyPostList";
 import MyPostView from "../components/MyPostView";
 import EditProfile from "../components/EditProfile";
+import MyPost from "../components/MyPost";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import { useDispatch } from "react-redux";
+import {
+  getClickPostView,
+  MyPostThumbsUp,
+} from "../module/ClickPostViewModule";
+import { getMyFeedList } from "../module/MyfeedPostListModule";
+import { getMyFeedInfo } from "../module/MyfeedPostListModule";
 import axios from "axios";
+import swal from "sweetalert";
 
 function MyFeed() {
+  const dispatch = useDispatch();
+
   const isEditProfile = useSelector((status: RootState) => {
     return status.isEditProfileReducer.isEditProfile;
   });
 
-  const [MyfeedInfo, setMyfeedInfo] = useState({
-    user_nickname: "",
-    user_img: null,
-    user_job: "",
-    user_comment: "",
-    total_learning_time: 0,
-    total_posting: 0,
-    postDatas: [],
+  // 클릭한 게시물의 데이터
+  const click_postview = useSelector((status: RootState) => {
+    return status.getClickPostViewReducer.click_postview;
   });
 
-  const {
-    user_nickname,
-    user_img,
-    user_job,
-    user_comment,
-    total_learning_time,
-    total_posting,
-    postDatas,
-  } = MyfeedInfo;
+  // 내 게시물 데이터
+  const MyFeedList = useSelector((status: RootState) => {
+    return status.getMyFeedListReducer.MyFeedList;
+  });
 
-  const getMyfeedInfo = function () {
-    axios
+  //현재 클릭한 게시물이 없다면(로그인하고 첫 myfeed에 들어간 상태라면)
+  //첫번째 게시물을 보여준다
+  const isChecked = function (firstPost: object) {
+    if (Object.keys(click_postview).length === 0) {
+      dispatch(getClickPostView(firstPost));
+    }
+  };
+
+  const getMyfeedInfo = async function () {
+    await axios
       .get(`http://localhost:4000/posts/myfeed`, {
         headers: {
           "Content-Type": "application/json",
@@ -53,41 +51,61 @@ function MyFeed() {
         withCredentials: true,
       })
       .then((res: any) => {
-        console.log("Myfeed: 전달받은 전체 데이터", res);
-        console.log("Myfeed: 게시물 목록 데이터", res.data.postDatas);
-        console.log("Myfeed: 유저데이터", res.data.userDatas);
-        const postDatas = res.data.postDatas;
-        const userDatas = res.data.userDatas;
-        // setMyfeedInfo를 이용하여 값을 저장한다
-        setMyfeedInfo({
-          user_nickname: userDatas.user_nickname,
-          user_img: userDatas.user_img,
-          user_job: userDatas.user_job,
-          user_comment: userDatas.profile_comment,
-          total_learning_time: userDatas.total_learning_time,
-          total_posting: userDatas.total_posting,
-          postDatas: postDatas,
-        });
+        const postDatas = res.data.postDatas; //배열
+        const userDatas = res.data.userDatas; //객체
+
+        // 게시물 데이터 저장
+        dispatch(getMyFeedList(postDatas.reverse() || {}));
+        // 사용자 정보 저장
+        dispatch(getMyFeedInfo(userDatas || {}));
+
+        console.log("사용자의 게시물 목록 데이터", postDatas);
+        console.log("사용자의 개인정보 데이터", userDatas);
+
+        isChecked(MyFeedList[0]);
+        searchThumbsUpHandler();
+
+        console.log("현재보여지는 데이터", click_postview);
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("my feed 데이터 가져오기 실패", "", "error");
+      });
+  };
+
+  const searchThumbsUpHandler = async function () {
+    await axios
+      .post(
+        "http://localhost:4000/posts/search-thumbsup",
+        { post_PK: click_postview.id },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        //해당 게시물 좋아요 유무 넘겨줌
+        dispatch(MyPostThumbsUp(res.data));
+        //해당 게시물 좋아요 개수 넘겨줌
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  useEffect(() => getMyfeedInfo(), []);
+  useEffect(() => {
+    // window.setTimeout(() => {
+    //   getMyfeedInfo();
+    // }, 2000);
+    getMyfeedInfo();
+  }, []);
 
-  // ######### My Feed 접속 시, 전체 페이지에 hooks에 저장한 데이터들을 각각의 컴포넌트에 props로 넘겨준다 #########
   return (
     <div id="MyFeed-container">
-      <MyProfileSidebar
-        user_nickname={user_nickname}
-        user_img={user_img}
-        user_job={user_job}
-        user_comment={user_comment}
-        total_learning_time={total_learning_time}
-        total_posting={total_posting}
-      />
-      <MyPostList postDatas={postDatas} />
+      <MyProfileSidebar />
+      <MyPostList />
       <MyPostView />
       {isEditProfile && <EditProfile />}
     </div>
