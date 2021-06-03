3e5e107e917001f5c1486e82ec486a7b740ed723
Jeong-HW <gusdnd114@naver.com>
2021-06-04 02:20:35 +0900

    업데이트: rootReducer에 새로 생성한 Reducer 추가

--- dawn-on-client/src/module/index.tsx
+++ dawn-on-client/src/module/index.tsx
@@ -4,6 +4,11 @@ import storage from "redux-persist/lib/storage";
 import isEditProfileReducer from "./EditProfileModule";
 import isLoginReducer from "./isLogin";
 import addTaskReducer from "./addTaskModule";
+import getClickPostViewReducer from "./ClickPostViewModule";
+import getClickExploreViewReducer from "./ClickExploreViewModule";
+import getExploreListReducer from "./ExplorePostListModule";
+import getRankingListReducer from "./ExploreRankingListModule";
+import getMyFeedListReducer from "./MyfeedPostListModule";
 const persistConfig = {
   key: "root", // reducer 객체에서 데이터를 저장하는 지점 설정
   storage, // 로컬 스토리지 사용
@@ -12,6 +17,11 @@ const persistConfig = {
 const rootReducer = combineReducers({
   isLoginReducer,
   addTaskReducer,
-  isEditProfileReducer
+  isEditProfileReducer,
+  getClickPostViewReducer,
+  getClickExploreViewReducer,
+  getExploreListReducer,
+  getRankingListReducer,
+  getMyFeedListReducer,
 });
 export default persistReducer(persistConfig, rootReducer);
