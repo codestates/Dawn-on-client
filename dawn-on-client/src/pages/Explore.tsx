89b7dd76a5f9601c7f00d92143a3c2203af28b58
Jeong-HW <gusdnd114@naver.com>
2021-06-04 16:37:25 +0900

    삭제: 주석

--- dawn-on-client/src/pages/Explore.tsx
+++ dawn-on-client/src/pages/Explore.tsx
@@ -1,22 +1,3 @@
-/*
-모아보기
-1. ExploreSidebar (  왼쪽 : 랭킹 및 잡다한거 보여는  사이드 바)
-2. ExploreList ( 중앙 : 다른 사용자들의 플래너 박스의 썸네일들이 보여지는 리스트 )
-    1. Select Box 존재 ( 리스트 내 게시물 필터링 기능)
-    2. ExplorePost ( 중앙 : 썸네일 리스트에 담겨있는 각각의 플래너 박스) 👉 [ ExploreList ]
-        1. Delete 버튼 X
-3. ExplorePostView ( 오른쪽 : 플래너 박스 클릭 시,  보여주는 상세 페이지)
-    1. Edit 버튼 X
-*/
-
-/* 
-ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-아이디 검색 기능과 아이디 클릭 기능은 같은 요청 처리로 해결 가능
-검색창에서 해시태그를 검색할 경우, 조건문으로 분기
-*/
-
-// 모아보기 게시물 초기값은 최신순, 전체 게시물
-
 import "../css/explore.css";
 import ExploreSidebar from "../components/ExploreSidebar";
 import ExploreList from "../components/ExploreList";
