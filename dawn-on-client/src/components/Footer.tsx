3c2b4d47f6585c3db6cecd0bb23a4736ebd3da50
Jeong-HW <gusdnd114@naver.com>
2021-06-07 02:29:17 +0900

    구현: 랜딩페이지 하단 footer

--- dawn-on-client/src/components/Footer.tsx
+++ dawn-on-client/src/components/Footer.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import "../css/footer.css";
+import github from "../img/github.png";
 
 function Footer() {
-  return <div id="footer">이 곳은 모든 페이지에 보여지게 될 Footer입니다</div>;
+  return (
+    <div id="footer">
+      <span id="Dawn-on">Dawn:on</span>
+      <span id="Done-up">Done:up @ 2021 Dawn:on </span>
+      <div id="github-list">
+        <img className="github-link" src={github} width="30" height="30" />
+        <span
+          className="github-link"
+          onClick={() => window.open("https://github.com/Hprogram", "_blank")}
+        >
+          전영호
+        </span>
+        <span
+          className="github-link"
+          onClick={() => window.open("https://github.com/gookgookJ", "_blank")}
+        >
+          장한국
+        </span>
+        <span
+          className="github-link"
+          onClick={() => window.open("https://github.com/allofhyuk", "_blank")}
+        >
+          권재혁
+        </span>
+        <span
+          className="github-link"
+          onClick={() => window.open("https://github.com/Jeong-HW", "_blank")}
+        >
+          정현웅
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
