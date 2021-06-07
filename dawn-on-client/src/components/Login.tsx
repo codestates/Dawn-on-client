649e365bc813c3456a842a08d89bfb27d27558bc
Jeong-HW <gusdnd114@naver.com>
2021-06-07 22:38:23 +0900

    수정: 로그인창 디자인 변경

--- dawn-on-client/src/components/Login.tsx
+++ dawn-on-client/src/components/Login.tsx
@@ -3,11 +3,15 @@ import swal from "sweetalert";
 import "../css/login.css";
 import "../App.css";
 import axios from "axios";
-import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { getLoginState } from "../module/isLogin";
 import styled, { keyframes } from "styled-components";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import kakaoTalk from "../img/KakaoTalk.png";
+import google from "../img/google.png";
+import "antd/dist/antd.css";
 
 const fadeIn = keyframes`
   from {
@@ -24,15 +28,13 @@ type LoginProps = {
 };
 
 const LoginContainer = styled.div`
-  display: grid;
-  grid-template-columns: 0.3fr 1fr 0.3fr;
-  grid-template-rows: 0.2fr 1fr 1fr 1fr 1fr 0.2fr;
-  background-color: #f5eded;
-  background-image: linear-gradient(217deg, #f5eded 0%, #ffffff 100%);
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px;
+  rgba(0, 0, 0, 0.05) 0px 5px 10px;
   opacity: 0.96;
   border-radius: 20px;
   width: 500px;
-  height: 450px;
+  height: 500px;
   margin: -225px 0 0 -225px;
   position: fixed;
   top: 50%;
@@ -50,29 +52,13 @@ const CloseButton = styled.button`
   justify-self: right;
   font-size: 1.5rem;
   margin-top: 10px;
-  color: #2b3390;
-  grid-column: 3 / 4;
-  grid-row: 1 / 2;
+  color: #2e4c8c;
 `;
 
 // 로그인 성공 시, Redux-persist에 해당 사용자의 정보, 로그인 상태값, accessToken을 저장시켜줘야한다
 function Login({ closeLoginModal, openJoinModal }: LoginProps) {
   const dispatch = useDispatch();
   const history = useHistory();
-
-  const [form, setForm] = useState({
-    user_id: "",
-    user_password: "",
-  });
-  const { user_id, user_password } = form;
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
 
   // 로그인 성공 시, 서버로부터 token을 받아온다
   async function Local_Login_getToken() {
@@ -115,8 +101,10 @@ function Login({ closeLoginModal, openJoinModal }: LoginProps) {
   };
 
   //로컬 로그인 함수
-  const Local_loginRequestHandler = async function () {
-    console.log("로그인한 사용자 정보", form);
+  const Local_loginRequestHandler = async function (
+    user_id: string,
+    user_password: string
+  ) {
     await axios
       .post(
         `${process.env.REACT_APP_URI}/auth/signin`,
@@ -137,88 +125,94 @@ function Login({ closeLoginModal, openJoinModal }: LoginProps) {
       .catch((err) => {
         console.log(err);
         swal("ID와 Password가 일치하지 않습니다", "", "error");
-        /*         Swal.fire({
-          title: "ID와 Password가 일치하지 않습니다",
-          width: 600,
-          padding: "3em",
-          background: "",
-          backdrop: `
-            rgba(0,0,123,0.4)
-            url("/images/nyan-cat.gif")
-            left top
-            no-repeat
-          `,
-        }); */
       });
+  };
+
+  // 로그인 창 form
+  const LoginForm = () => {
+    const onFinish = (values: any) => {
+      console.log("입력한 회원가입 정보", values);
+      const { user_id, user_password } = values;
+      Local_loginRequestHandler(user_id, user_password);
+    };
+
+    return (
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <span className="login-title">Log In</span>
+        <Form.Item
+          name="user_id"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="User ID"
+          />
+        </Form.Item>
+        <Form.Item
+          name="user_password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item style={{ textAlign: "center", justifyContent: "center" }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            style={{ height: "40px" }}
+          >
+            Log in
+            <i className="fas fa-sign-in-alt"></i>
+          </Button>
+          <span
+            className="go-to-join"
+            onClick={() => {
+              openJoinModal();
+              closeLoginModal();
+            }}
+          >
+            아직 회원이 아니신가요?
+          </span>
+        </Form.Item>
+        <Button
+          className="social-btn"
+          id="kakao"
+          onClick={kakakoLogins}
+          style={{ height: "40px" }}
+        >
+          <img src={kakaoTalk} alt="카카오톡 이미지" className="social-img" />
+          Kakao LogIn
+        </Button>
+        <Button
+          className="social-btn"
+          id="google"
+          onClick={googleLogins}
+          style={{ height: "40px" }}
+        >
+          <img src={google} alt="구글 이미지" className="social-img" />
+          Google LogIn
+        </Button>
+      </Form>
+    );
   };
 
   return (
     <>
       <div>
-        <LoginContainer id="a">
+        <LoginContainer>
           <CloseButton className="close-btn" onClick={() => closeLoginModal()}>
             <i className="far fa-times-circle"></i>
           </CloseButton>
-          <div id="login-title">LOG IN</div>
-          <div id="login-input-container">
-            <span id="login-input">ID</span>
-
-            <input
-              className="login-input-id"
-              name="user_id"
-              value={user_id}
-              placeholder="your ID"
-              onChange={onChange}
-            />
-            <span id="login-input">PASSWORD</span>
-
-            <input
-              className="login-input-pw"
-              name="user_password"
-              placeholder="your password"
-              value={user_password}
-              type="password"
-              onChange={onChange}
-            />
-          </div>
-          <div id="login-join">
-            <button
-              id="login-to-join"
-              onClick={() => {
-                openJoinModal();
-                closeLoginModal();
-              }}
-            >
-              아직 회원이 아니신가요?
-            </button>
-
-            <button
-              id="login-btn"
-              onClick={() => {
-                Local_loginRequestHandler();
-              }}
-            >
-              LOG IN
-            </button>
-          </div>
-          <div id="login-btn-container">
-            <button
-              id="google-btn"
-              onClick={() => {
-                googleLogins();
-              }}
-            >
-              google 로그인
-            </button>
-            <button
-              id="kakao-btn"
-              onClick={() => {
-                kakakoLogins();
-              }}
-            >
-              kakao 로그인
-            </button>
-          </div>
+          <LoginForm />
         </LoginContainer>
       </div>
     </>
