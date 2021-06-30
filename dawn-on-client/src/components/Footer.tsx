860e76fdfbbba9352392f8c57ca3e01f4ed90ebe
allofhyuk <gggg21367@gmail.com>
2021-06-30 17:37:15 +0900

    리팩토링: Footer 반응형 적용

--- dawn-on-client/src/components/Footer.tsx
+++ dawn-on-client/src/components/Footer.tsx
@@ -1,19 +1,32 @@
 import "../css/footer.css";
 import github from "../img/github.png";
+import { Popover, Button } from 'antd';
 
 function Footer() {
+  const frontEnd = (
+    <div>
+      <p>Frontend</p>
+    </div>
+  );
+  const backEnd = (
+    <div>
+      <p>Backend</p>
+    </div>
+  );
   return (
     <div id="footer">
-      <span id="Dawn-on">Dawn:on</span>
-      <span id="Done-up">Done:up @ 2021 Dawn:on </span>
+      <div id="Dawn-on">
+        Dawn:on<br></br>
+         Done:up @ 2021 Dawn:on
+      </div>
       <div id="github-list">
         <img className="github-link" src={github} width="30" height="30" />
-        <span
-          className="github-link"
-          onClick={() => window.open("https://github.com/Hprogram", "_blank")}
-        >
-          전영호
-        </span>
+          <span
+            className="github-link"
+            onClick={() => window.open("https://github.com/Hprogram", "_blank")}
+          >
+            전영호
+          </span>
         <span
           className="github-link"
           onClick={() => window.open("https://github.com/gookgookJ", "_blank")}
